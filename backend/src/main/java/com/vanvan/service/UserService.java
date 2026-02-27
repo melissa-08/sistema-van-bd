9a285fdde28301555cafb440b76f4ba22d816be6
baracholeticia <leticiabaracho06@gmail.com>
2026-02-27 15:35:47 -0300

    fix: conserta erros relacionados a admin no back

--- backend/src/main/java/com/vanvan/service/UserService.java
+++ backend/src/main/java/com/vanvan/service/UserService.java
@@ -4,85 +4,94 @@ import com.vanvan.dto.RegisterDTO;
 import com.vanvan.exception.UnderageUserException;
 import com.vanvan.exception.CnhAlreadyExistsException;
 import com.vanvan.exception.UnderageDriverException;
+import com.vanvan.model.Driver;
+import com.vanvan.model.User;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
 import com.vanvan.exception.CpfAlreadyExistsException;
 import com.vanvan.exception.EmailAlreadyExistsException;
-import com.vanvan.model.Driver;
+import com.vanvan.model.Administrator;
 import com.vanvan.model.Passenger;
-import com.vanvan.model.User;
+import com.vanvan.repository.AdministratorRepository;
 import com.vanvan.repository.DriverRepository;
 import com.vanvan.repository.PassengerRepository;
 import com.vanvan.repository.UserRepository;
-
-import lombok.RequiredArgsConstructor;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.Period;
 
 @Service
-@RequiredArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
-    private final DriverRepository driverRepository;
-    private final PassengerRepository passengerRepository;
-    private final PasswordEncoder passwordEncoder;
+    private UserRepository userRepository;
+    private DriverRepository driverRepository;
+    private PassengerRepository passengerRepository;
+    private AdministratorRepository administratorRepository;
+    private PasswordEncoder passwordEncoder;
 
     public User register(RegisterDTO data) {
 
         var user = data.toEntity();
 
-        // Faz todas as validações aqui e retorna exceptions personalizadas caso falhe
+        //faz todas validações aqui e retorna runtimeexceptions personalizadas caso falhe
         validateUser(user);
 
         // Criptografa a senha
         String encryptedPassword = passwordEncoder.encode(user.getPassword());
         user.setPassword(encryptedPassword);
 
-        // Faz o switch pelo tipo de usuário (Admin foi removido daqui!)
+        // Faz o switch pelo tipo de usuário
         return switch (user.getRole()) {
             case PASSENGER -> {
                 assert user instanceof Passenger;
                 yield passengerRepository.save((Passenger) user);
             }
+            case ADMIN -> {
+                assert user instanceof Administrator;
+                yield administratorRepository.save((Administrator) user);
+            }
             case DRIVER -> {
                 assert user instanceof Driver;
                 yield driverRepository.save((Driver) user);
             }
-            default -> throw new IllegalArgumentException("Operação não permitida: Tipo de usuário inválido para esta rota.");
         };
     }
 
-    // Método extraído para cá para melhor separação de responsabilidades
+    //métod0 extraído para cá para melhor separação de responsabilidades
     private void validateUser(User user) {
 
-        // Valida idade
+        //valida idade
         validateAge(user);
 
-        // Verifica se o e-mail já está cadastrado
+        //verifica se o e-mail já está cadastrado
         if (userRepository.findByEmail(user.getEmail()) != null) {
             throw new EmailAlreadyExistsException(user.getName());
         }
         // Verifica se o CPF já está cadastrado
         else if (userRepository.findByCpf(user.getCpf()) != null) {
             throw new CpfAlreadyExistsException(user.getCpf());
-        } 
-        // Verifica CNH em caso de driver
-        else if (user instanceof Driver driver && driverRepository.existsByCnh(driver.getCnh())) {
+            //verifica cnh em caso de driver
+        } else if (user instanceof Driver driver && driverRepository.existsByCnh(driver.getCnh())) {
             throw new CnhAlreadyExistsException(driver.getCnh());
         }
     }
 
-    // Validação de idade
-    private void validateAge(User user) {
+    //validação de idade
+    private void validateAge(User user){
         int age = Period.between(user.getBirthDate(), LocalDate.now()).getYears();
 
         if (user instanceof Driver && age < 21) {
              throw new UnderageDriverException();
         }
-        if (age < 18) {
+        if (age < 18){
             throw new UnderageUserException();
         }
+
     }
+
+
 }
